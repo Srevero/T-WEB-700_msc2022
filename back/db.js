ab0232f7ff6df185a113e5fcaa23f1467290e5aa
breizhsurfer <boistuaud.hugo@gmail.com>
2020-11-13 11:34:51 +0100

    [ADD] -back "Init docker back"

---
+++ back/db.js
@@ -0,0 +1,9 @@
+var mysql      = require('mysql');
+var connection = mysql.createPool({
+    host     : 'money-mysql',
+    user     : 'admin',
+    port     : '3306',
+    password : 'cashIsLife',
+    database : 'countOfMoney'
+});
+module.exports=connection;
