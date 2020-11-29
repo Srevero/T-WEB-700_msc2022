7f97d5bee0aadfe473b63c33c0e7aef68d59cafb
axelbrasseur <axel.brasseur@epitech.eu>
2020-11-29 21:53:23 +0100

    [ADD] -front static register component

--- front/src/Views/Register.vue
+++ front/src/Views/Register.vue
@@ -3,7 +3,41 @@
   <div>
     <Header></Header>
     <Footer></Footer>
-    <p> Je suis le register </p>
+    <v-card class="registerCard">
+      <h1> Register </h1>
+      <v-text-field
+        type="username"
+        placeholder="username"
+        class="textFieldRegister"
+        solo
+        dense
+      ></v-text-field>
+      <v-text-field
+        type="email"
+        placeholder="email"
+        class="textFieldRegister"
+        solo
+        dense
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        placeholder="password"
+        class="textFieldRegister"
+        solo
+        dense
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        placeholder="confirm password"
+        class="textFieldRegister"
+        solo
+        dense
+      ></v-text-field>
+      <v-btn
+        elevation="2"
+        class="btnRegister"
+      > Register </v-btn>
+    </v-card>
   </div>
 </template>
 
@@ -17,5 +51,29 @@ export default {
 </script>
 
 <style scoped>
+ .registerCard {
+   background-color: #F9C81D;
+   height: 70%;
+   width: 30%;
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+ }
 
+ h1 {
+   margin-top: 30px;
+   text-align: center;
+ }
+
+  .textFieldRegister {
+    margin: 20px;
+    background-color: #F9C81D;
+  }
+
+  .btnRegister {
+    position: absolute;
+    margin-bottom: 40px;
+    left: 40%;
+  }
 </style>
